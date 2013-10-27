fec73c636d8ba934178bb8f4e3631e9c84ca248a
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2013-10-26 22:19:52 -0400

    Privatized a few things

--- aws3.js
+++ aws3.js
@@ -1,26 +1,44 @@
 /* jshint laxcomma: true */
 
+module.exports = exports = Aws3;
+
+
 var crypto = require('crypto')
   ;
-
-
-module.exports = exports = Aws3;
 
 
 Aws3.awsAccessKey = process.env.AWS_ACCESS_KEY_ID;
 Aws3.awsSecretKey = process.env.AWS_SECRET_ACCESS_KEY;
 Aws3.s3Bucket     = process.env.S3_BUCKET || 'aws3_bucket';
 
+function Aws3(file_name, mime_type, path, acl, expire_in) {
+  var awsAccessKey = Aws3.awsAccessKey
+    , awsSecretKey = Aws3.awsSecretKey
+    , s3Bucket     = Aws3.s3Bucket
+    ;
 
-function Aws3(file_name, mime_type, acl, path) {
-  this.awsAccessKey = Aws3.awsAccessKey;
-  this.awsSecretKey = Aws3.awsSecretKey;
-  this.s3Bucket     = Aws3.s3Bucket;
-  this.file_name    = file_name;
-  this.mime_type    = mime_type;
-  this.acl          = acl || 'private';
-  this.path         = path || '/';
-  this.expire_in    = 3600;
+  this.file_name = file_name;
+  this.mime_type = mime_type;
+  this.acl       = acl || 'private';
+  this.path      = path || '/';
+  this.expire_in = expire_in || 3600;
+
+
+  this.s3_url = function() {
+    return 'https://s3.amazonaws.com/'+s3Bucket;
+  };
+
+  this.bucket_file_path = function() {
+    return '/'+s3Bucket+'/'+this.file_path();
+  };
+
+
+  this.generate_signed_request = function(method) {
+    return [
+        this.url()
+      , key_and_expires_params()+'&Signature='+signature(method)
+    ].join('?');
+  };
 
 
   var self = this;
@@ -29,13 +47,13 @@ function Aws3(file_name, mime_type, acl, path) {
     return 'x-amz-acl:'+self.acl;
   };
 
-  this.req_string = function(method) {
+  var payload = function(method) {
     var arr = [
         method.toUpperCase()
       , ''
-      , (method==='put' ? this.mime_type : '')
-      , this.expires_in()
-      , this.bucket_file_path()
+      , (method==='put' ? self.mime_type : '')
+      , self.expires_in()
+      , self.bucket_file_path()
     ];
 
     if (method === 'put') arr.splice(4, 0, amzaclheader());
@@ -43,40 +61,25 @@ function Aws3(file_name, mime_type, acl, path) {
     return arr.join('\n').toString('utf-8');
   };
 
-  this.signature = function(method) {
+  var signature = function(method) {
     return crypto
-      .createHmac('sha1', this.awsSecretKey)
-      .update(this.req_string(method))
+      .createHmac('sha1', awsSecretKey)
+      .update(payload(method))
       .digest('base64');
   };
 
-  this.generate_signed_request = function(method) {
-    return [
-        this.url()
-      , this.key_and_expires_params()+'&Signature='+this.signature(method)
-    ].join('?');
-  };
-
-  this.key_and_expires_params = function() {
-    return 'AWSAccessKeyId='+this.awsAccessKey+'&Expires='+this.expires_in();
+  var key_and_expires_params = function() {
+    return 'AWSAccessKeyId='+awsAccessKey+'&Expires='+self.expires_in();
   };
 }
 
-
-Aws3.prototype.base_url = function() {
-  return 'https://s3.amazonaws.com/'+this.s3Bucket;
-};
 
 Aws3.prototype.file_path = function() {
   return (this.path.replace(/\/$/, '')+'/'+this.file_name).replace(/^\//, '');
 };
 
-Aws3.prototype.bucket_file_path = function() {
-  return '/'+this.s3Bucket+'/'+this.file_path();
-};
-
 Aws3.prototype.url = function() {
-  return this.base_url()+'/'+this.file_path();
+  return this.s3_url()+'/'+this.file_path();
 };
 
 Aws3.prototype.expires_in = function() {
--- test/aws3_test.js
+++ test/aws3_test.js
@@ -24,9 +24,11 @@ describe('Aws3', function() {
   });
 
 
-  it('generates a base_url using the s3Bucket', function() {
-    var aws3 = new Aws3('large.txt', 'text/plain');
-    assert.equal(aws3.base_url(), 'https://s3.amazonaws.com/aws3_bucket');
+  describe('#s3_url', function() {
+    it('generates the s3 url w/ bucket', function() {
+      var aws3 = new Aws3('large.txt', 'text/plain');
+      assert.equal(aws3.s3_url(), 'https://s3.amazonaws.com/aws3_bucket');
+    });
   });
 
   it('can define these arguements', function() {
@@ -35,10 +37,12 @@ describe('Aws3', function() {
     assert.equal(aws3.mime_type, 'text/plain');
     assert.equal(aws3.acl, 'private');
     assert.equal(aws3.path, '/');
+    assert.equal(aws3.expire_in, 3600);
 
-    aws3 = new Aws3('large.txt', 'text/plain', 'public', '/a/new/path');
+    aws3 = new Aws3('large.txt', 'text/plain', '/a/new/path', 'public', 7200);
     assert.equal(aws3.acl, 'public');
     assert.equal(aws3.path, '/a/new/path');
+    assert.equal(aws3.expire_in, 7200);
   });
 
   it('expires in 60 minutes by default', function() {
@@ -47,28 +51,28 @@ describe('Aws3', function() {
   });
 
   describe('#file_path', function() {
-    it('returns the generated file_path', function() {
-      var aws3 = new Aws3('large.txt', 'text/plain', null, '/generated-unique-key');
-      assert.equal(aws3.file_path(), 'generated-unique-key/large.txt');
+    it('returns the full file path', function() {
+      var aws3 = new Aws3('large.txt', 'text/plain', '/a-unique-key');
+      assert.equal(aws3.file_path(), 'a-unique-key/large.txt');
     });
   });
 
   describe('#url', function() {
     it('returns the generated url', function() {
-      var aws3 = new Aws3('large.txt', 'text/plain', null, '/generated-unique-key');
-      assert.equal(aws3.url(), 'https://s3.amazonaws.com/aws3_bucket/generated-unique-key/large.txt');
+      var aws3 = new Aws3('large.txt', 'text/plain', '/a-unique-key');
+      assert.equal(aws3.url(), 'https://s3.amazonaws.com/aws3_bucket/a-unique-key/large.txt');
     });
   });
 
   describe('#bucket_file_path', function() {
     it('returns the full file path w/ bucket', function() {
-      var aws3 = new Aws3('large.txt', 'text/plain', null, '/generated-unique-key');
-      assert.equal(aws3.bucket_file_path(), '/aws3_bucket/generated-unique-key/large.txt');
+      var aws3 = new Aws3('large.txt', 'text/plain', '/a-unique-key');
+      assert.equal(aws3.bucket_file_path(), '/aws3_bucket/a-unique-key/large.txt');
     });
   });
 
-  describe('signed requests', function() {
-    var aws3 = new Aws3('large.txt', 'text/plain', null, '/abcd/');
+  describe('#signed_url', function() {
+    var aws3;
 
     function signature(str) {
       return crypto.createHmac('sha1', process.env.AWS_SECRET_ACCESS_KEY)
@@ -76,27 +80,50 @@ describe('Aws3', function() {
         .digest('base64');
     }
 
-    describe('#signed_url', function() {
-      it('returns a signed get request', function() {
-        var arr = ['GET', '', '', one_hour_from_now(), '/aws3_bucket/abcd/large.txt'].join('\n');
-        assert.equal(aws3.req_string('get'), arr);
-        assert.equal(aws3.signed_url('get'), 
-                     aws3.url()+'?'+aws3.key_and_expires_params()+'&Signature='+signature(arr));
-      });
+    function url() {
+      return 'https://s3.amazonaws.com/aws3_bucket/abcd/large.txt?AWSAccessKeyId='+
+        process.env.AWS_ACCESS_KEY_ID+'&Expires='+one_hour_from_now();
+    }
 
-      it('returns a signed put request', function() {
-        var arr = ['PUT', '', 'text/plain', one_hour_from_now(), 'x-amz-acl:private', '/aws3_bucket/abcd/large.txt'].join('\n');
-        assert.equal(aws3.req_string('put'), arr);
-        assert.equal(aws3.signed_url('put'), 
-                     aws3.url()+'?'+aws3.key_and_expires_params()+'&Signature='+signature(arr));
-      });
+    beforeEach(function() {
+      aws3 = new Aws3('large.txt', 'text/plain', '/abcd/');
+    });
 
-      it('returns a signed delete request', function() {
-        var arr = ['DELETE', '', '', one_hour_from_now(), '/aws3_bucket/abcd/large.txt'].join('\n');
-        assert.equal(aws3.req_string('delete'), arr);
-        assert.equal(aws3.signed_url('delete'), 
-                     aws3.url()+'?'+aws3.key_and_expires_params()+'&Signature='+signature(arr));
-      });
+    it('returns a signed get request', function() {
+      var arr = [
+          'GET'
+        , ''
+        , ''
+        , one_hour_from_now()
+        , '/aws3_bucket/abcd/large.txt'
+      ].join('\n');
+
+      assert.equal(aws3.signed_url('get'), url()+'&Signature='+signature(arr));
+    });
+
+    it('returns a signed put request', function() {
+      var arr = [
+          'PUT'
+        , ''
+        , 'text/plain'
+        , one_hour_from_now()
+        , 'x-amz-acl:private'
+        , '/aws3_bucket/abcd/large.txt'
+      ].join('\n');
+
+      assert.equal(aws3.signed_url('put'), url()+'&Signature='+signature(arr));
+    });
+
+    it('returns a signed delete request', function() {
+      var arr = [
+          'DELETE'
+        , ''
+        , ''
+        , one_hour_from_now()
+        , '/aws3_bucket/abcd/large.txt'
+      ].join('\n');
+
+      assert.equal(aws3.signed_url('delete'), url()+'&Signature='+signature(arr));
     });
   });
 });
